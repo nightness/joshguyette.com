f8005cb3e1cd620ee1f6b5ef4631dc9017e1ad7e
Josh Guyette <joshguyette@yahoo.com>
2023-07-03 11:42:24 -0500

    added primereact

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import './App.css'
+import { PrimeReactProvider } from "primereact/api";
+import "./App.css";
+
+import { InputText } from "primereact/inputtext";
 
 function App() {
   return (
-    <>
-
+    <PrimeReactProvider>
       <h1>Joshua Guyette's Portfolio</h1>
       <p className="read-the-docs">
         Site is under construction. Please check back later.
       </p>
-    </>
-  )
+      <InputText />
+    </PrimeReactProvider>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// PrimeReact
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+
+// Main
+import App from "./App.tsx";
+import "./index.css";
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
