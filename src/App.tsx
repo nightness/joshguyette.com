cd0286855fc3be2a1f2284eb7d4d58f81bba91a8
Josh Guyette <joshguyette@yahoo.com>
2023-07-03 12:30:22 -0500

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,54 @@
+import { useState } from "react";
 import { PrimeReactProvider } from "primereact/api";
+import { Dialog } from "primereact/dialog";
+import { Button } from "primereact/button";
+
 import "./App.css";
 
-import { InputText } from "primereact/inputtext";
-
 function App() {
+  const [visible, setVisible] = useState(false);
+
   return (
     <PrimeReactProvider>
       <h1>Joshua Guyette's Portfolio</h1>
-      <p className="read-the-docs">
-        Site is under construction. Please check back later.
-      </p>
-      <InputText />
+      <Button
+        label="About"
+        className="p-button-raised p-button-rounded"
+        style={{ marginRight: "1rem" }}
+        onClick={() => setVisible(true)}
+      />
+      <Button
+        label="Resume"
+        className="p-button-raised p-button-rounded"
+        style={{ marginRight: "1rem" }}
+        onClick={() =>
+          window.open("https://joshguyette.com/resume.pdf", "_blank")
+        }
+      />
+      <Button
+        label="LinkedIn"
+        className="p-button-raised p-button-rounded"
+        style={{ marginRight: "1rem" }}
+        onClick={() =>
+          window.open(
+            "https://www.linkedin.com/in/joshua-guyette-854311113/",
+            "_blank"
+          )
+        }
+      />
+      <Button
+        label="GitHub"
+        className="p-button-raised p-button-rounded"
+        onClick={() => window.open("https://github.com/nightness", "_blank")}
+      />
+      <Dialog
+        header="About Me"
+        visible={visible}
+        style={{ width: "50vw" }}
+        onHide={() => setVisible(false)}
+      >
+        <p className="m-0">Work in progress</p>
+      </Dialog>
     </PrimeReactProvider>
   );
 }
